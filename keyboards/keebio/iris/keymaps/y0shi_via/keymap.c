c424dd42b327251604418271f24f611cf904b7d8
Joshua Reddick <y0shi@noreply.github.com>
2025-09-03 11:02:47 -0400

    update RGB matrix configuration and layer color handling

--- keyboards/keebio/iris/keymaps/y0shi_via/keymap.c
+++ keyboards/keebio/iris/keymaps/y0shi_via/keymap.c
@@ -5,36 +5,44 @@ enum {
     _MEDIA
 };
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    // Determine the current layer
-    uint8_t layer = get_highest_layer(layer_state | default_layer_state);
-
-    // Apply color based on the layer
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch (layer) {
-            case _QWERTY:
-                // Base layer: Purple underglow
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 128, 0, 128); // RGB values for purple
-                break;
-            case _RAISE:
-                // Raise layer: Red underglow
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 255, 0, 0); // RGB values for red
-                break;
-            case _LOWER:
-                // Lower layer: Green underglow
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 0, 255, 0); // RGB values for green
-                break;
-            case _MEDIA:
-                // media layer: Blue underglow
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 0, 0, 255); // RGB values for blue
-                break;
-            default:
-                // Default color for any other layers (or no specific layer active)
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 0, 0, 0); // RGB values for off (or another default)
-                break;
-        }
+void set_layer_rgb(uint8_t layer) {
+    if (!is_keyboard_master()) {
+        return;
     }
+    
+    switch (layer) {
+        case _QWERTY:
+            // Base layer: Purple
+            rgb_matrix_sethsv_noeeprom(213, 255, 128); // Purple in HSV
+            break;
+        case _RAISE:
+            // Raise layer: Red
+            rgb_matrix_sethsv_noeeprom(0, 255, 255); // Red in HSV
+            break;
+        case _LOWER:
+            // Lower layer: Green
+            rgb_matrix_sethsv_noeeprom(85, 255, 255); // Green in HSV
+            break;
+        case _MEDIA:
+            // Media layer: Blue
+            rgb_matrix_sethsv_noeeprom(170, 255, 255); // Blue in HSV
+            break;
+        default:
+            // Default - turn off
+            rgb_matrix_sethsv_noeeprom(0, 0, 0);
+            break;
+    }
+}
 
-    return true;
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = get_highest_layer(state | default_layer_state);
+    set_layer_rgb(layer);
+    return state;
+}
+
+void keyboard_post_init_user(void) {
+    // Initialize RGB matrix to solid color mode and set initial layer color
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    set_layer_rgb(_QWERTY);
 }
 
